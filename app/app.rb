92008ebbc53d854c4746b782f1467703cd22e220
Michael Minton <michaelcminton@gmail.com>
2013-01-04 21:21:53 -0500

    Improve readability.

--- app/app.rb
+++ app/app.rb
@@ -13,39 +13,50 @@ module Spot
     get '/' do
       'Welcome to Spot!<br/>http://github.com/minton/Spot'
     end
+
     put '/play' do
       Player.volume = 45
       Player.play
     end
+
     put '/pause' do
       Player.pause
     end
+
     put '/mute' do
       Player.mute
     end
+
     get '/playing' do
      Player.playing
     end
+
     put '/next' do
       Player.next
     end
+
     put '/back' do
       Player.back
     end
+
     get '/volume' do
       Player.volume.to_s
     end
+
     put  '/volume' do
       Player.volume = params[:volume].to_i and Player.volume.to_s unless params[:volume].nil?
     end
+
     put '/bumpup' do
       Player.volume = bump_up_volume.to_i
       Player.volume.to_s
     end
+
     put '/bumpdown' do
       Player.volume = Player.volume - bump_down_volume.to_i
       Player.volume.to_s
     end
+
     post '/find' do
       query = params[:q]  
       track_uri = Spotify.find(query)
@@ -55,35 +66,40 @@ module Spot
         Player.play_song(track_uri)
       end 
     end
+
     get '/playing.png' do
       content_type 'image/png'
       img = Player.artwork
       send_file img, :disposition => 'inline'
     end
+
     private
-    def bump_up_volume
-      current_volume = Player.volume
-      case current_volume
-      when 0..10
-        20
-      when 11..30
-        current_volume*1.45
-      when 31..70
-        current_volume*1.25
-      else
-        current_volume*1.1
+
+      def bump_up_volume
+        current_volume = Player.volume
+        case current_volume
+        when 0..10
+          20
+        when 11..30
+          current_volume*1.45
+        when 31..70
+          current_volume*1.25
+        else
+          current_volume*1.1
+        end
       end
-    end
-    def bump_down_volume
-      current_volume = Player.volume
-      case current_volume
-      when 100..80
-        current_volume*0.3
-      when 79..40
-        current_volume*0.2
-      else
-        current_volume*0.1
+
+      def bump_down_volume
+        current_volume = Player.volume
+        case current_volume
+        when 100..80
+          current_volume*0.3
+        when 79..40
+          current_volume*0.2
+        else
+          current_volume*0.1
+        end
       end
-    end
+      
   end
 end
