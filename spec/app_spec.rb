fb656e9041f3d8efed65b0233c1a4c2f44d6b578
Michael Minton <michaelcminton@gmail.com>
2013-01-28 15:36:59 -0500

    start app spec.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 module Spot
   describe App do
     describe "GET /" do
-      it "should return a welcome" do
+      it "should greet us" do
         get "/"
         last_response.should be_ok
+        last_response.body.should == "Welcome to Spot!<br/>http://github.com/minton/Spot"
       end 
     end
   end
